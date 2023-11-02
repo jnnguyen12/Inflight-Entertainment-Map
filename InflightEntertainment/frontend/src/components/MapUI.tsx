09698c0212788c055bde9e599b790cbe9320daa8
Joanne Nguyen <jnnguyen@iastate.edu>
2023-11-02 16:07:47 -0500

    created mapUI

---
+++ InflightEntertainment/frontend/src/components/MapUI.tsx
@@ -0,0 +1,16 @@
+/**
+ * The purpose of this container is to provide UI controls for the map.
+ * If RND container is not at full device screen width and height, then the small screen UI is returned.
+ * If RND container is at full screen, then the full screen UI is returned.
+ */
+
+import React from 'react'
+
+const MapUI = () => {
+    
+  return (
+    <div>MapUI</div>
+  )
+}
+
+export default MapUI
