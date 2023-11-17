cf8832df949ef5add5a17678e1980645120c8323
randyvan12 <randyvan12@gmail.com>
2023-11-16 23:38:09 -0600

    fixed small error

--- InflightEntertainment/api/urls.py
+++ InflightEntertainment/api/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('addPolylinePayload/', views.addPolyline, name="addPolyline"),
     path('removePolylinePayload/', views.removePolyline, name="removePolyline"),
 
-    path('timezone/', views.get_timezone, name="getTimezone"),
+    path('timezone/', views.getTimezone, name="getTimezone"),
 #    path('WellnessCheck/', views.wellnessCheck, name="Wellness Check")
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
